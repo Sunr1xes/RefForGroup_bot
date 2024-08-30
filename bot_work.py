af413199225451bcc0efa93dae26c93e9eeac4da
Sunr1xes <mr.yamb34@gmail.com>
2024-08-31 00:15:54 +0300

    security API

--- bot_work.py
+++ bot_work.py
@@ -1,20 +1,44 @@
+import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.filters import Command
+from dotenv import load_dotenv
 
-API_TOKEN = "7175695555:AAEQZofXMkqZyTVdkZdXjENIv_6MGgazL7A"
+# Подключение API
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
+# Проверка, что API_KEY не None
+if API_KEY is None:
+    raise ValueError(
+        "Переменная окружения API_KEY не установлена. Пожалуйста, проверьте файл .env."
+    )
+
+# Логгирование
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=API_KEY)
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=["start"])
+# Обработчик команды /start
+@dp.message(Command("start"))
 async def send_welcome(message: types.Message):
-    await message.replay(message.text)
+    await message.answer("Привет! Я бот, как я могу помочь вам сегодня?")
+
+
+async def main():
+    # Регистрация обработчиков
+    dp.message.register(send_welcome, Command("start"))
+
+    # Запуск polling
+    await bot.delete_webhook(
+        drop_pending_updates=True
+    )  # Удаление старого вебхука (если был установлен)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
