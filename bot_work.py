9b516e34d7ddd47e93d29c7e0368fa125034bff2
Sunr1xes <mr.yamb34@gmail.com>
2024-08-30 23:36:44 +0300

    dev

--- bot_work.py
+++ bot_work.py
@@ -1,12 +1,20 @@
-import aiogram
+import logging
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from aiogram.types import ParseMode
-import logging 
 
-API_TOKEN = '7175695555:AAEQZofXMkqZyTVdkZdXjENIv_6MGgazL7A'
+API_TOKEN = "7175695555:AAEQZofXMkqZyTVdkZdXjENIv_6MGgazL7A"
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def send_welcome(message: types.Message):
+    await message.replay(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
